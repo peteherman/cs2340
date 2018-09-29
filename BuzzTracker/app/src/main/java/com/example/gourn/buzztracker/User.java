e97a857c5bd7908b771f82fe312a36cae3ade780
peteherman <peteherman@yahoo.com>
2018-09-29 19:29:00 -0400

    Added Getters and Setter to User class

--- BuzzTracker/app/src/main/java/com/example/gourn/buzztracker/User.java
+++ BuzzTracker/app/src/main/java/com/example/gourn/buzztracker/User.java
@@ -13,4 +13,37 @@ public class User {
         this.password = password;
         this.userType = userType;
     }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public UserType getUserType() {
+        return userType;
+    }
+
+    public void setUserType(UserType userType) {
+        this.userType = userType;
+    }
+
 }
