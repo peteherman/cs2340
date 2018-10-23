21476b52a7ba49b7e73cf30a0dc9d8f8d792fdbb
peteherman <peteherman@yahoo.com>
2018-10-23 12:05:42 -0400

    Cleaned up Detailed Donation view

--- BuzzTracker/app/src/main/java/com/example/gourn/buzztracker/Controller/DetailedDonationView.java
+++ BuzzTracker/app/src/main/java/com/example/gourn/buzztracker/Controller/DetailedDonationView.java
@@ -55,16 +55,15 @@ public class DetailedDonationView extends AppCompatActivity {
         Query query = databaseReference.child("donations").child(locationName).child(donationId);
         query.addValueEventListener(new ValueEventListener() {
             private HashMap<String, String> donationInfo = new HashMap<>();
+            private final int YEAR_OFFSET = 1;
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                List<String> donationValues = new ArrayList<>();
                 for (DataSnapshot d : dataSnapshot.getChildren()) {
                     donationInfo.put(d.getKey().toString(), d.getValue().toString());
+                    donationValues.add(formatDonation(d));
                 }
 
-                List<String> donationValues = new ArrayList<>();
-                for (String dv : donationInfo.values()) {
-                    donationValues.add(dv);
-                }
 
                 ArrayAdapter<String> adapter = new ArrayAdapter<>(DetailedDonationView.this,
                         android.R.layout.simple_list_item_1, android.R.id.text1, donationValues);
@@ -75,6 +74,36 @@ public class DetailedDonationView extends AppCompatActivity {
             @Override
             public void onCancelled(@NonNull DatabaseError databaseError) {
 
+            }
+
+            public String formatDonation(DataSnapshot d) {
+                String result = "";
+
+                String key = d.getKey();
+                String value = d.getValue().toString();
+
+                result += key + ": ";
+
+                if (key != null && key.equalsIgnoreCase("timestamp")) {
+                    String timestampString = "";
+                    timestampString += d.child("month").getValue().toString();
+                    timestampString += "-";
+                    timestampString += d.child("day").getValue().toString();
+                    timestampString += "-";
+                    timestampString += d.child("year").
+                            getValue().toString().substring(YEAR_OFFSET);
+                    timestampString += "  ";
+                    timestampString += d.child("hours").getValue().toString();
+                    timestampString += ":";
+                    timestampString += d.child("minutes").getValue().toString();
+                    timestampString += ":";
+                    timestampString += d.child("seconds").getValue().toString();
+
+                    result += timestampString;
+                } else {
+                    result += value;
+                }
+                return result;
             }
         });
     }
